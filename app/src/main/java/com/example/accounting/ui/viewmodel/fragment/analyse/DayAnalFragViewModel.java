fdecca5786df2a8ce2b3095e4fbad83e76796a8c
Lxiaowai <1251625424@qq.com>
2023-07-01 23:13:40 +0800

    analize

--- app/src/main/java/com/example/accounting/ui/viewmodel/fragment/analyse/DayAnalFragViewModel.java
+++ app/src/main/java/com/example/accounting/ui/viewmodel/fragment/analyse/DayAnalFragViewModel.java
@@ -1,11 +1,38 @@
 package com.example.accounting.ui.viewmodel.fragment.analyse;
+import android.graphics.Color;
 
+import com.example.accounting.base.BaseActivityViewModel;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.example.accounting.base.BaseFragmentViewModel;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
 
 public class DayAnalFragViewModel extends BaseFragmentViewModel
 {
     public DayAnalFragViewModel()
     {
         super();
+    }
+    public static BarData generateBarChartData() {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 100));
+        entries.add(new BarEntry(1, 200));
+        entries.add(new BarEntry(2, 150));
+        entries.add(new BarEntry(3, 300));
+        entries.add(new BarEntry(4, 250));
+        entries.add(new BarEntry(5, 180));
+        entries.add(new BarEntry(6, 120));
+
+        BarDataSet dataSet = new BarDataSet(entries, "花销");
+        dataSet.setColor(Color.parseColor("#87CEFA"));
+        dataSet.setValueTextColor(Color.BLUE);
+        return new BarData(dataSet);
     }
 }
--- app/src/main/java/com/example/accounting/ui/viewmodel/fragment/analyse/MonthAnalFragViewModel.java
+++ app/src/main/java/com/example/accounting/ui/viewmodel/fragment/analyse/MonthAnalFragViewModel.java
@@ -1,11 +1,39 @@
 package com.example.accounting.ui.viewmodel.fragment.analyse;
 
+import android.graphics.Color;
+
 import com.example.accounting.base.BaseFragmentViewModel;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 public class MonthAnalFragViewModel extends BaseFragmentViewModel
 {
     public MonthAnalFragViewModel()
     {
         super();
+    }
+    public static BarData generateBarChartData() {
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 1000));
+        entries.add(new BarEntry(1, 2000));
+        entries.add(new BarEntry(2, 1500));
+        entries.add(new BarEntry(3, 3000));
+        entries.add(new BarEntry(4, 2500));
+        entries.add(new BarEntry(5, 1800));
+        entries.add(new BarEntry(6, 1200));
+        entries.add(new BarEntry(7, 2000));
+        entries.add(new BarEntry(8, 1500));
+        entries.add(new BarEntry(9, 3000));
+        entries.add(new BarEntry(10, 2500));
+        entries.add(new BarEntry(11, 1800));
+        entries.add(new BarEntry(12, 1200));
+
+        BarDataSet dataSet = new BarDataSet(entries, "花销");
+        dataSet.setColor(Color.parseColor("#87CEFA"));
+        dataSet.setValueTextColor(Color.BLUE);
+        return new BarData(dataSet);
     }
 }
