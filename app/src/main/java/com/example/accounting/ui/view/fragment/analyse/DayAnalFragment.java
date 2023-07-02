b4042ae9fc8a6a2d99e6cf27dd5deca348be1bc3
bbslzy <bbslzy001@henu.edu.cn>
2023-07-02 19:28:25 +0800

    update day_anal.

--- app/src/main/java/com/example/accounting/ui/view/fragment/analyse/DayAnalFragment.java
+++ app/src/main/java/com/example/accounting/ui/view/fragment/analyse/DayAnalFragment.java
@@ -1,23 +1,12 @@
 package com.example.accounting.ui.view.fragment.analyse;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.lifecycle.ViewModelProvider;
-
 import com.example.accounting.BR;
 import com.example.accounting.R;
 import com.example.accounting.base.BaseFragment;
 import com.example.accounting.databinding.FragmentAnalDayBinding;
 import com.example.accounting.ui.viewmodel.fragment.analyse.DayAnalFragViewModel;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 public class DayAnalFragment extends BaseFragment<FragmentAnalDayBinding, DayAnalFragViewModel>
@@ -39,24 +28,44 @@ public class DayAnalFragment extends BaseFragment<FragmentAnalDayBinding, DayAna
     {
         return BR.viewModel;
     }
-    private BarChart mBarChart;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_anal_day, container, false);
+    protected void initView()
+    {
+        super.initView();
+        initIncomeChart();
+        initExpenseChart();
+    }
 
-        mBarChart = view.findViewById(R.id.bar_chart);
-
-        DayAnalFragViewModel viewModel = new ViewModelProvider(this).get(DayAnalFragViewModel.class);
-        BarData barData = DayAnalFragViewModel.generateBarChartData();
-        XAxis xAxis = mBarChart.getXAxis();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]{"Mon", "Tues", "Weds", "Thurs", "Fri","Sat","Sun"}));
+    private void initIncomeChart()
+    {
+        // 设置x轴
+        XAxis xAxis = binding.incomeChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]{"工资", "奖金"}));
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        mBarChart.setData(barData);
-        mBarChart.invalidate();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]{"周一", "周二", "周三", "周四", "周五", "周六", "周日"}));
 
+        // 填充数据
+        binding.incomeChart.setData(viewModel.getIncomeData());
 
-        return view;
+        // 设置空白描述
+        Description description = new Description();
+        description.setText("");
+        binding.incomeChart.setDescription(description);
+    }
+
+    private void initExpenseChart()
+    {
+        // 设置x轴
+        XAxis xAxis = binding.expenseChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(new String[]{"餐饮", "交通", "购物", "娱乐"}));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        // 填充数据
+        binding.expenseChart.setData(viewModel.getExpenseData());
+
+        // 设置空白描述
+        Description description = new Description();
+        description.setText("");
+        binding.expenseChart.setDescription(description);
     }
 }
